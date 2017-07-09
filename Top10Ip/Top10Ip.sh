247d8d419634c43f7d34fbf2877bbf014a03a220
qiongweirengithub <qiongwei_ren@163.com>
2017-07-09 17:33:42 +0800

    Add files via upload

--- Top10Ip/Top10Ip.sh
+++ Top10Ip/Top10Ip.sh
@@ -1,4 +1,17 @@
-# calculate top 10 ip access
-#!/bin/bash
+#! /bin/bash
 
-awk '{print $1}' /opt/apache-tomcat-8.5.14/logs/localhost_access_log.* |sort| uniq -c | sort -rn | head -n 10
+	echo -n '' > ./IpRecord.txt
+	for filename in /usr/local/qunar/tomcat7/apache-tomcat-7.0.75/logs/localhost_access_log.*.txt	
+	do
+		while read line
+		do
+			echo ${line%% *} >> ./IpRecord.txt
+		done < $filename
+	done
+
+	awk -F ' ' 'BEGIN   {a=1} {array[$1]++;}
+				 END	{for(e in array){print e,array[e] | "sort -r -n -k2"; } }' ./IpRecord.txt
+
+	rm ./IpRecord.txt
+
+
